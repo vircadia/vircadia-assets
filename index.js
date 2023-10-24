f97e2ff13b5f0fac31ec307f3f5a51663b6e6731
Giga <gigabyte5671@gmail.com>
2023-10-24 16:22:12 +1300

    Add icons

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import * as audio from "./audio/index.js";
+import * as icons from "./icons/index.js";
 import * as models from "./models/index.js";
 
 export const directories = {
 	audio,
+	icons,
 	models
 };
 
