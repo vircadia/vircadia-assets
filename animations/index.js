ccff2dca35810d29691fbaf0ffe9932637f0e495
Giga <gigabyte5671@gmail.com>
2023-10-24 16:42:41 +1300

    Add animation assets

---
+++ animations/index.js
@@ -0,0 +1,5 @@
+export const directories = {};
+
+export const files = [
+	"AnimationsBasic.glb"
+];
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import * as animations from "./animations/index.js";
 import * as audio from "./audio/index.js";
 import * as icons from "./icons/index.js";
 import * as models from "./models/index.js";
 import * as video from "./video/index.js";
 
 export const directories = {
+	animations,
 	audio,
 	icons,
 	models,
