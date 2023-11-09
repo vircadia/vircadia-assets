b29bbb8a72565736027c98aa2a082851af9c6187
Giga <gigabyte5671@gmail.com>
2023-11-09 21:52:43 +1300

    Add Vircadia Web text logo

--- images/branding/index.ts
+++ images/branding/index.ts
@@ -4,5 +4,7 @@ export const files = [
     "1920_bar.png",
     "og_banner.png",
     "vircadia-icon-256.png",
-    "vircadia-icon.svg"
+    "vircadia-icon.svg",
+    "vircadia_web_text_logo.ai",
+    "vircadia_web_text_logo.svg"
 ] as const;
